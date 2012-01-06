2aa70b99fd1081b2ac353f5639e17eebb5c227b9
Jack Senechal <jack@jacksenechal.com>
2012-01-05 20:46:51 -0500

    [fixes #22936601] pure html tree now supports dynamically expanding partners to the right

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -5,8 +5,13 @@ module PeopleHelper
 
   def children_with_partner(partner)
     if !partner.nil?
+      children_html = []
       @person.children_with(partner).each do |child|
-        concat(tree_node child)
+        children_html.push tree_node(child)
+      end
+      if !children_html.empty?
+        separator_html = '<div class="node_separator"></div>'
+        concat (separator_html + children_html.join(separator_html)).html_safe
       end
     end
     nil
@@ -14,7 +19,7 @@ module PeopleHelper
 
   def children_with_unknown
     @person.children_with_unknown.each do |child|
-      concat(tree_node child)
+      concat tree_node(child)
     end
   end
 end
